1ed98fd378d7b7b434bb10b4caf57c42d5c369ad
Vishnu Dawah <vkdawah@gmail.com>
2019-04-28 17:44:39 -0400

    Created routers for users, profile, and posts js files

---
+++ devConnector/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://blurlingite:vdj03@devconnector-zul53.mongodb.net/test?retryWrites=true"
+};
---
+++ devConnector/routes/api/posts.js
@@ -0,0 +1,18 @@
+const express = require("express"); // we need express to use routers
+
+const router = express.Router();
+
+// we can do router.post() etc. depending on what kind of HTTP you need to use
+
+// we don't need to change "/test" since the url will be /api/profile now
+
+// @route GET api/posts/test
+// @desc  Tests posts route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+// res.json() will output json. Automatically serves a status of 200, which means everything is ok. We will use Postman to test it
+
+// we have to export the router in order for server.js to pick it up
+module.exports = router;
+
+// test out this URL in the browser to see if you get the JSON message http://localhost:5000/api/posts/test
